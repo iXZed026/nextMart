4fc0c43138710dfeed6b7593de693cc688bf807b
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-20 16:53:26 +0330

    refactor: removing Container component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { ChildrenProps } from '@/types/children'
 import Navbar from '../navbar/Navbar'
-import Container from '../container/Container'
 
 function Layout({ children }: ChildrenProps) {
     return (
         <>
             <Navbar />
-            <Container>
-                {children}
-            </Container>
+            {children}
         </>
     )
 }
