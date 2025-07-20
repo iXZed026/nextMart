13f53f590e91eea30bc9a4728ec9a6b08d4c19bc
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-20 14:52:48 +0330

    feat: Add active link feature

--- src/components/navbar/navlinks/NavLinks.tsx
+++ src/components/navbar/navlinks/NavLinks.tsx
@@ -1,21 +1,45 @@
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 
+interface ILinks {
+    id: number,
+    title: string,
+    href: string,
+}
+
 function NavLinks() {
+
+    const pathname = usePathname();
+
+    const listStyle: string = 'text-gray-800 text-lg font-bold'
+
+    const links: ILinks[] = [
+        {
+            id: 1,
+            title: 'Home',
+            href: '/'
+        },
+        {
+            id: 2,
+            title: 'Shop',
+            href: '/shop'
+        },
+    ]
+
     return (
         <nav>
             <ul className='flex items-center gap-8 text-md font-medium'>
-                <li>
-                    <Link href="/">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/shop">
-                        Shop
-                    </Link>
-                </li>
-
+                {
+                    links.map((link, key) => (
+                        <li key={link.id} className={pathname === link.href ? listStyle : ''}>
+                            <Link href={link.href}>
+                                {link.title}
+                            </Link>
+                        </li>
+                    ))
+                }
             </ul>
         </nav>
     )
