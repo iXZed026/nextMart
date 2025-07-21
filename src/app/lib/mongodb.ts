7e1b2dc3bdb0f4bb168d945b746a3f3a29efa118
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-21 16:32:42 +0330

    fear: add mongodb.ts file

---
+++ src/app/lib/mongodb.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const uri = process.env.MONGODB_URI as string
+if (!uri) throw new Error("Check your conection string");
+
+let isConnected: boolean = false;
+
+export async function connectToDatabase() {
+    if (isConnected) console.log("Database is already connected.")
+    try {
+        mongoose.connect(uri, { dbName: "nextmart" })
+        isConnected = true;
+    }
+    catch (err) {
+        console.log("An error occurred while connecting to the database. ", err)
+        process.exit(1)
+    }
+
+}
