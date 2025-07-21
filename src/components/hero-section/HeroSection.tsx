b7b51c03f0f71404faa534d8c581594ca3747292
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-21 14:34:25 +0330

    feat: add and complete HeroSection component

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -1,17 +1,39 @@
 import Image from 'next/image'
 import React from 'react'
+import Container from '../container/Container'
 
 function HeroSection() {
     return (
-        <div>
-            <div>
-                <h2>Next Mart Shop</h2>
-                <p>This website was built with next.js for resume by Danyal Lotfi</p>
-                <button>View products</button>
-            </div>
-            <div>
-                <Image src="/images/hero.jpeg" width={400} height={300} alt='Electronic image' />
-            </div>
+        <div className='bg-white'>
+            <Container>
+                <div className='h-[450px] flex justify-between items-center'>
+                    <div>
+                        <h1 className='mb-8 text-3xl font-bold'>Next Mart Shop</h1>
+                        <p
+                            className='mb-10 text-md text-[var(--secound-text-colort)] w-full lg:w-2/3'
+                        >
+                            This website was built with Next.js, React, and TypeScript.
+                            Designed to showcase my skills in modern web development.
+                        </p>
+                        <button
+                            className='button'
+                        >
+                            View products
+
+                        </button>
+                    </div>
+                    <div>
+                        <div className='relative w-[450px] h-[300px]'>
+                            <Image
+                                className='rounded-lg'
+                                src="/images/hero.jpeg"
+                                fill
+                                alt='Electronic image'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Container>
         </div>
     )
 }
