e875e3c8190830c3476de7f21a2373708536a907
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-22 11:37:43 +0330

    feat:add products intreface

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -2,10 +2,19 @@ import { connectToDatabase } from "@/app/lib/mongodb";
 import Product from "../../../../model/Product";
 import { NextResponse } from "next/server";
 
+export interface IProduct {
+    _id: string,
+    title: string,
+    description: string,
+    price: number,
+    category: string,
+    image: string
+}
+
 export async function GET() {
     try {
         await connectToDatabase();
-        const products = await Product.find({})
+        const products = await Product.find({}) as IProduct[];
         return NextResponse.json(products, { status: 200 })
     }
     catch (err) {
