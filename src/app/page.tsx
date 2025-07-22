3b9d31b60382c439edfcdf2009cae64f663be2b7
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-22 11:39:58 +0330

    feat: import Products in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/hero-section/HeroSection'
+import Products from '@/components/products/Products'
 import React from 'react'
 
 function Home() {
   return (
     <div>
       <HeroSection />
+      <Products />
     </div>
   )
 }
