af2ae45e671347f0bbf94eebde068a835e35f880
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-23 17:39:15 +0330

    feat: create Loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='h-[200px] flex justify-center items-center'>
+        <div className='loading animate-spin'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Loading
