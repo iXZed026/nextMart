961ad472cd2259d674278e4719a4d116b0c39742
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-23 16:57:32 +0330

    feat: building an API to find products

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,21 @@
+import { connectToDatabase } from "@/app/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import Product from "../../../../../model/Product";
+
+interface IProductRouteParams {
+    params: { id: string }
+}
+
+export async function GET(req: NextRequest, { params }: IProductRouteParams) {
+
+    const id = await params.id;
+    try {
+        await connectToDatabase();
+        const product = await Product.findOne({ _id: id })
+        return NextResponse.json(product, { status: 200 })
+    }
+    catch (err) {
+        return NextResponse.json({ message: err }, { status: 500 })
+    }
+
+}
