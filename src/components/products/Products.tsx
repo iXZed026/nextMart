2ab9b41c659bacb7c068f387a1c7187c0b6e347a
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-23 17:40:32 +0330

    feat: add suspense feature

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,27 +1,17 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Container from '../container/Container'
-import ProductBox from './product-box/ProductBox'
-import { IProduct } from '@/app/api/products/route'
+import ProductsContent from './product-content/ProductsContent'
+import Loading from '../loading/Loading'
 
-async function Products() {
-
-    const resullt = await fetch("http://localhost:3000/api/products", {
-        next:{
-            revalidate:20,
-        }
-    })
-    const products = await resullt.json();
+function Products() {
 
     return (
         <Container>
             <div className='py-10'>
                 <h2 className='font-semibold text-2xl text-center py-2 mb-5'>Products</h2>
-                <div className="grid grid-cols-12 gap-14">
-                    {
-                        products.map((product: IProduct, key: string) => <ProductBox key={product._id} {...product} />)
-                    }
-                </div>
-
+                <Suspense fallback={<Loading />}>
+                    <ProductsContent />
+                </Suspense>
             </div>
         </Container>
     )
---
+++ src/components/products/product-content/ProductsContent.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ProductBox from '../product-box/ProductBox';
+import { IProduct } from '@/app/api/products/route';
+
+async function ProductsContent() {
+
+    const resullt = await fetch("http://localhost:3000/api/products", {
+        next: {
+            revalidate: 20,
+        }
+    })
+    const products = await resullt.json();
+    return (
+        <div className="grid grid-cols-12 gap-14">
+            {
+                products.map((product: IProduct, key: string) => <ProductBox key={product._id} {...product} />)
+            }
+        </div>
+    )
+}
+
+export default ProductsContent
