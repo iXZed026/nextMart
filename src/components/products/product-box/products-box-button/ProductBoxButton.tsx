8782f71fa3edc137019c27fdcbe0b4046be9e8d4
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-24 10:29:53 +0330

    feat: complete add to cart in home page

---
+++ src/components/products/product-box/products-box-button/ProductBoxButton.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { IProduct } from '@/app/api/products/route'
+import { ICart, useCartContext } from '@/context/CartProvider'
+import React from 'react'
+
+function ProductBoxButton(props: IProduct) {
+
+    const { cart, setCart } = useCartContext();
+
+    function addToCartHandler() {
+
+        const findProduct = cart.find((cart: IProduct) => cart._id === props._id);
+
+        if (findProduct) {
+            setCart((prevCart: ICart[]) => (
+                prevCart.map((item: any) => {
+                    if (item._id === findProduct._id) {
+                        return { ...item, count: item.count + 1 };
+                    }
+                    return item
+                })
+            )
+            )
+        } else {
+            setCart((prevCart: ICart[]) => {
+                return [...prevCart, { ...props, count: 1 }];
+            })
+        }
+
+    }
+
+    return (
+        <button onClick={addToCartHandler} className='button'>Add To Cart</button >
+    )
+}
+
+export default ProductBoxButton
