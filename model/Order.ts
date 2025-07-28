e9357adaa5bc80782218841b06ec63b434d1a2e2
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-28 11:01:34 +0330

    feat: create new model Order.ts

--- model/Order.ts
+++ model/Order.ts
@@ -1,31 +1,46 @@
 import mongoose from "mongoose";
 
-const date = new Date();
-const FullDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
-
 const OrderSchema = new mongoose.Schema({
     user: {
-        name: String,
-        email: String,
-        country: String,
-        city: String,
-        address: String,
-        postalCode: String,
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+        },
+        country: {
+            type: String,
+            required: [true, "Country is required"],
+        },
+        city: {
+            type: String,
+            required: [true, "City is required"],
+        },
+        address: {
+            type: String,
+            required: [true, "Address is required"],
+        },
+        postalCode: {
+            type: String,
+            required: [true, "Postal code is required"],
+        },
     },
-    cart: [{
-        _id: String,
-        title: String,
-        description: String,
-        price: Number,
-        category: String,
-        image: String,
-        count: Number,
-    }],
-
-    totalPrice: Number,
-    status: { type: String, default: "pending" },
-    createAt: { type: String, default: FullDate },
-
-})
+    cart: [
+        {
+            _id: { type: String, required: true },
+            title: { type: String, required: true },
+            description: { type: String, required: true },
+            price: { type: Number, required: true },
+            category: { type: String, required: true },
+            image: { type: String, required: true },
+            count: { type: Number, required: true },
+        },
+    ],
+    totalPrice: { type: Number, required: true },
+    status: { type: String, default: "pending", required: true },
+    createdAt: { type: Date, default: Date.now, required: true },
+});
 
 export default mongoose.models.Order || mongoose.model("Order", OrderSchema)
