9eccac25817736b6cb7edeefb79d85b6d5e6c238
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-28 11:02:01 +0330

    feat: adding Order data in api

--- src/app/api/order/route.tsx
+++ src/app/api/order/route.tsx
@@ -4,7 +4,7 @@ import Order from "../../../../model/Order";
 
 export async function POST(req: NextRequest) {
     try {
-        const { user, cart,totalPrice } = await req.json();
+        const { user, cart, totalPrice } = await req.json();
         console.log(totalPrice)
         await connectToDatabase();
         const newOrder = new Order({
@@ -13,9 +13,8 @@ export async function POST(req: NextRequest) {
             totalPrice,
         });
         await newOrder.save();
-        return NextResponse.json({ data: newOrder ,message:"Request is successfully!"},{status:200});
+        return NextResponse.json({ data: newOrder, message: "Request is successfully!" }, { status: 200 });
     } catch (err) {
-        console.error("Order Save Error:", err);
-        return NextResponse.json({ data: null, message: err }, { status: 500 });
+        return NextResponse.json(err, { status: 500 });
     }
 }
