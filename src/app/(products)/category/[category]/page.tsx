9f56a0df56976ad4ee2a8c899f937fe19f4ac33b
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-29 13:00:03 +0330

    feat:create category page route

---
+++ src/app/(products)/category/[category]/page.tsx
@@ -0,0 +1,29 @@
+import { IProduct } from '@/app/api/products/route';
+import Container from '@/components/container/Container';
+import ProductBox from '@/components/products/product-box/ProductBox';
+import React from 'react';
+
+interface ICategory {
+    params: Promise<{ category: string }>
+}
+
+async function ProductCategory({ params }: ICategory) {
+
+    const { category } = await params;
+
+    const result = await fetch(`http://localhost:3000/api/category/${category}`);
+    const productsCategory = await result.json();
+
+
+    return (
+        <Container>
+            <div className='grid grid-cols-12 gap-15 py-20'>
+                {
+                    productsCategory.map((item: IProduct, key: string) => <ProductBox {...item} key={item._id} />)
+                }
+            </div>
+        </Container>
+    )
+}
+
+export default ProductCategory
