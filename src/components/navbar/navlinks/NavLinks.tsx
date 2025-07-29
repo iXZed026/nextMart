8dd9560473ee68939c4cb93cc3ff28df56cf5e08
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-29 14:23:10 +0330

    refactor: refactoring navbar component

--- src/components/navbar/navlinks/NavLinks.tsx
+++ src/components/navbar/navlinks/NavLinks.tsx
@@ -13,7 +13,7 @@ function NavLinks() {
 
     const pathname = usePathname();
 
-    const listStyle: string = 'text-gray-800 text-lg font-bold'
+    const listStyle: string = 'link-style font-semibold'
 
     const links: ILinks[] = [
         {
@@ -21,20 +21,15 @@ function NavLinks() {
             title: 'Home',
             href: '/'
         },
-        {
-            id: 2,
-            title: 'Shop',
-            href: '/shop'
-        },
     ]
 
     return (
         <nav>
-            <ul className='flex items-center gap-8 text-md font-medium'>
+            <ul className='flex items-center gap-12 text-md font-medium'>
                 {
                     links.map((link, key) => (
-                        <li key={link.id} className={pathname === link.href ? listStyle : ''}>
-                            <Link href={link.href}>
+                        <li key={link.id} >
+                            <Link href={link.href} className={`px-3 py-1 ${pathname === link.href ? listStyle : ''}`}>
                                 {link.title}
                             </Link>
                         </li>
