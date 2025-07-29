680545c2a4a5b0b3ddc4ec4fb571c589009f9436
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-29 09:51:15 +0330

    feat: create and complete Footer.tsx compomemt

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Container from '../container/Container'
+
+function Footer() {
+    return (
+        <footer className='bg-[var(--main-color)] py-20'>
+            <Container>
+                <div className="grid grid-cols-12 text-gray-200 text-lg">
+                    <div className="col-span-4">
+                        <h2 className='text-xl decoration-2 font-bold mb-7'>Customer Service</h2>
+                        <ul>
+                            <li className='mb-3 cursor-pointer'>Contact Us</li>
+                            <li className='mb-3 cursor-pointer'>FAQs</li>
+                            <li className='mb-3 cursor-pointer'>Shipping & Delivery</li>
+                            <li className='mb-3 cursor-pointer'>Returns & Exchanges </li>
+                            <li className='mb-3 cursor-pointer'>Track Order</li>
+                        </ul>
+                    </div>
+                    <div className="col-span-4">
+                        <h2 className='text-xl decoration-2 font-bold mb-7'>Our Store</h2>
+                        <ul>
+                            <li className='mb-3 cursor-pointer'>About Us</li>
+                            <li className='mb-3 cursor-pointer'>Store Locator</li>
+                            <li className='mb-3 cursor-pointer'>Careers</li>
+                            <li className='mb-3 cursor-pointer'>Blog</li>
+                            <li className='mb-3 cursor-pointer'>Affiliate Program</li>
+                        </ul>
+                    </div>
+                    <div className="col-span-4">
+                        <h2 className='text-xl decoration-2 font-bold mb-7'>Follow Us</h2>
+                        <ul>
+                            <li className='mb-3 cursor-pointer'>Facebook</li>
+                            <li className='mb-3 cursor-pointer'>Instagram</li>
+                            <li className='mb-3 cursor-pointer'>Twitter</li>
+                            <li className='mb-3 cursor-pointer'>YouTube</li>
+                            <li className='mb-3 cursor-pointer'>TikTok</li>
+                        </ul>
+                    </div>
+                </div>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
