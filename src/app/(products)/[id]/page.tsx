f0c5bb6c670825cd0150750611e201e391b44c50
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-29 12:59:41 +0330

    feat:add category Link

--- src/app/(products)/[id]/page.tsx
+++ src/app/(products)/[id]/page.tsx
@@ -2,6 +2,7 @@ import { IProductRouteParams } from '@/app/api/products/[id]/route';
 import Container from '@/components/container/Container'
 import ProductBoxButton from '@/components/products/product-box/add-to-cart-button/AddToCartButton';
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 
 async function Product({ params }: IProductRouteParams) {
@@ -29,7 +30,10 @@ async function Product({ params }: IProductRouteParams) {
                 </div>
                 <div className="col-span-8 relative py-10a">
                     <h2 className='text-2xl font-bold mb-5'>{product.title}</h2>
-                    <h4 className='text-[var(--secound-text-color)] mb-5'>Category: <span>{product.Category}</span></h4>
+                    <h4 className='text-[var(--secound-text-color)] mb-5'>
+                        <Link href={`/category/${product.category}`}>
+                            <span className='font-bold text-[var(--main-color)]'>{product.category}</span></Link>
+                    </h4>
                     <p className='mb-5 font-semibold'>{product.description}.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ad aspernatur assumenda praesentium, quod facilis placeat sequi nemo quam ab qui unde labore! Fugiat illum labore consequuntur corporis quas nemo?</p>
                     <div className='flex items-center gap-5 absolute bottom-3'>
                         <span>${product.price}</span>
