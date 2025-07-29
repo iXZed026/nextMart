fd69ecc08207b05e3f438a301157db7f780bb3e4
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-29 15:15:33 +0330

    feat/style: complete HeroSection component responsive

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -5,11 +5,11 @@ function HeroSection() {
     return (
         <div className='bg-white'>
             <Container>
-                <div className='h-[450px] flex justify-between items-center'>
+                <div className='py-[90px] flex flex-col md:flex-row justify-between items-center md:gap-0 gap-15'>
                     <div>
                         <h1 className='mb-8 text-3xl font-bold'>Next Mart Shop</h1>
                         <p
-                            className='mb-10 text-md text-[var(--secound-text-colort)] w-full lg:w-2/3'
+                            className='mb-10 text-md text-[var(--secound-text-colort)] lg:w-4/5 sm:w-full'
                         >
                             This website was built with Next.js, React, and TypeScript.
                             Designed to showcase my skills in modern web development.
@@ -22,9 +22,9 @@ function HeroSection() {
                         </button>
                     </div>
                     <div>
-                        <div className='w-[450px] h-[300px]'>
+                        <div className='flex md:justify-end items-center'>
                             <img
-                                className='rounded-lg'
+                                className='rounded-lg lg:w-[65%] md:w-[80%] w-full'
                                 src="/images/hero.jpeg"
                                 alt='Electronic image'
                             />
