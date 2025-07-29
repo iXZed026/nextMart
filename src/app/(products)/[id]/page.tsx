08045ab3969ba175fbbba043fce5c0c71de7f65e
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-29 23:40:11 +0330

    feat: responsive product detail

--- src/app/(products)/[id]/page.tsx
+++ src/app/(products)/[id]/page.tsx
@@ -15,26 +15,25 @@ async function Product({ params }: IProductRouteParams) {
 
     return (
         <Container>
-            <div className='grid grid-cols-12 gap-19 py-10 h-full'>
-                <div className="col-span-4 flex justify-center py-10 items-center bg-white rounded-xl">
+            <div className='grid grid-cols-12 gap-5  py-10 max-w-full'>
+                <div className=" xl:col-span-4 col-span-12 flex justify-center py-10 items-center bg-white rounded-xl">
                     <Image
                         src={product.image}
-                        width={0}
-                        height={0}
+                        width={300}
+                        height={300}
                         sizes='100vw'
-                        quality={65}
+                        quality={60}
                         alt='product image'
                         priority={true}
-                        style={{ width: "300px" }}
                     />
                 </div>
-                <div className="col-span-8 relative py-10a">
+                <div className="xl:col-span-8 col-span-12 relative py-10">
                     <h2 className='text-2xl font-bold mb-5'>{product.title}</h2>
                     <h4 className='text-[var(--secound-text-color)] mb-5'>
                         <Link href={`/category/${product.category}`}>
                             <span className='font-bold text-[var(--main-color)]'>{product.category}</span></Link>
                     </h4>
-                    <p className='mb-5 font-semibold'>{product.description}.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ad aspernatur assumenda praesentium, quod facilis placeat sequi nemo quam ab qui unde labore! Fugiat illum labore consequuntur corporis quas nemo?</p>
+                    <p className='mb-8 font-semibold'>{product.description}.Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ad aspernatur assumenda praesentium, quod facilis placeat sequi nemo quam ab qui unde labore! Fugiat illum labore consequuntur corporis quas nemo?</p>
                     <div className='flex items-center gap-5 absolute bottom-3'>
                         <span>${product.price}</span>
                         <ProductBoxButton {...product} />
