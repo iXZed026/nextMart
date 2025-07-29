95b38797d03092c916ad8efa397a78a3bb44ac3e
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-29 09:51:37 +0330

    feat: import Footer component in Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ChildrenProps } from '@/types/children'
 import Navbar from '../navbar/Navbar'
 import CartProvider from '@/context/CartProvider'
+import Footer from '../footer/Footer'
 
 function Layout({ children }: ChildrenProps) {
     return (
@@ -9,6 +10,7 @@ function Layout({ children }: ChildrenProps) {
             <CartProvider>
                 <Navbar />
                 {children}
+                <Footer />
             </CartProvider>
         </>
     )
