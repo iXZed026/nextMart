6901f03487b69ddbb2051296b9701992abe6f66b
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-07-30 15:19:20 +0330

    style: changing width style

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,7 +6,7 @@ import CartForm from '@/components/cart-form/CartForm'
 
 function Cart() {
     return (
-        <div className='xl:w-[75%] md:w-[80%] w-full mx-auto'>
+        <div className='xl:w-[75%] sm:w-[80%] w-full mx-auto'>
             <div className="w-full grid grid-cols-12 gap-7 mt-20">
                 <div className="cart-box xl:col-span-7 col-span-12">
                     <h2 className='mb-3 text-xl font-bold'>Cart</h2>
