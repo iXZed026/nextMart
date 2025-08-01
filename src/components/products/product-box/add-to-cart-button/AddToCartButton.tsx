9d956dc744b13720ce4abf99b10df5e30ed43b06
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-01 13:14:29 +0330

    feat/fix:fix modal bug and import ErrorModal component

--- src/components/products/product-box/add-to-cart-button/AddToCartButton.tsx
+++ src/components/products/product-box/add-to-cart-button/AddToCartButton.tsx
@@ -1,17 +1,20 @@
 "use client"
 import { IProduct } from '@/app/api/products/route'
+import ErrorModal from '@/components/error-modal/ErrorModal';
 import SuccessModal from '@/components/modal/success-modal/SuccessModal';
 import { ICart, useCartContext } from '@/context/CartProvider'
 import { usePathname, useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function AddToCartButton(props: IProduct) {
+
 
     const { cart, setCart } = useCartContext();
     const router = useRouter();
     const pathname = usePathname()
 
     const [showModal, setShowModal] = useState<boolean>(false)
+    const [showErrorModal, setShowErrorModal] = useState(false)
 
     function addToCartHandler() {
 
@@ -20,7 +23,7 @@ function AddToCartButton(props: IProduct) {
         if (findProduct) {
             setCart((prevCart: ICart[]) => (
                 prevCart.map((item) => {
-                    if (item._id === findProduct._id) {
+                    if (item._id === findProduct._id && item.count < 5) {
                         return { ...item, count: item.count + 1 };
                     }
                     return item
@@ -33,11 +36,26 @@ function AddToCartButton(props: IProduct) {
             })
         }
 
-        if (pathname !== "/") {
-            return router.push("/")
+        showModalHandler(findProduct?.count)
+
+    }
+
+    function showModalHandler(COUNT: any) {
+        if (COUNT < 5 || COUNT === undefined) {
+            if (pathname !== "/") {
+                setShowModal(true);
+                setTimeout(() => {
+                    setShowModal(false)
+                    router.push("/");
+                }, 1000)
+            } else {
+                setShowModal(true);
+                setTimeout(() => setShowModal(false), 1000)
+            }
         } else {
-            setShowModal(true);
-            setTimeout(() => setShowModal(false), 4000)
+            setShowModal(false)
+            setShowErrorModal(true)
+            setTimeout(() => setShowErrorModal(false), 1000)
         }
 
     }
@@ -52,6 +70,7 @@ function AddToCartButton(props: IProduct) {
                 Add To Cart
             </button >
             <SuccessModal showModal={showModal} title={`${props.title}, Added to cart!`} />
+            <ErrorModal showErrorModal={showErrorModal} title={`You cannot add more than 5 of these products to your cart.`} />
         </>
     )
 }
