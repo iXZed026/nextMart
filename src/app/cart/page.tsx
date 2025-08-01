368519f93b8be41192e2ebc84d4ae811f283427a
Danyal-windows <danyal.titanka24@gmail.com>
2025-08-01 12:09:28 +0330

    feat: add next-mart-logo in metaData

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -6,14 +6,14 @@ import CartForm from '@/components/cart-form/CartForm'
 
 function Cart() {
     return (
-        <div className='xl:w-[75%] sm:w-[80%] w-full mx-auto'>
-            <div className="w-full grid grid-cols-12 gap-7 mt-20">
+        <div className='xl:w-[75%] sm:w-[80%] w-full mx-auto my-20'>
+            <div className="w-full grid grid-cols-12 gap-7">
                 <div className="cart-box xl:col-span-7 col-span-12">
-                    <h2 className='mb-3 text-xl font-bold'>Cart</h2>
+                    <h2 className='mb-3 text-lg font-semibold'>Cart</h2>
                     <CartTable />
                 </div>
                 <div className="cart-box xl:col-span-5 col-span-12 h-[600px]">
-                    <h2 className='mb-3 text-xl font-bold'>Your information</h2>
+                    <h2 className='mb-3 text-lg font-semibold'>Your information</h2>
                     <CartForm />
                 </div>
             </div>
