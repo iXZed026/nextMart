09f7cb5d302539f809bcf4d2faa56a9d7b8eb067
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-03 14:44:51 +0330

    test: test

--- src/components/products/product-content/ProductsContent.tsx
+++ src/components/products/product-content/ProductsContent.tsx
@@ -2,23 +2,24 @@ import React from 'react'
 import ProductBox from '../product-box/ProductBox';
 import { IProduct } from '@/app/api/products/route';
 
-export const vercelDomain = JSON.parse(process.env.NEXT_PUBLIC_BASE_URL as string)
+export const vercelDomain = process.env.NEXT_PUBLIC_BASE_URL as string;
+
 
 async function ProductsContent() {
 
-        const resullt = await fetch(`${vercelDomain}/api/products`, {
-            next: {
-                revalidate: 20,
+    const resullt = await fetch(`${vercelDomain}/api/products`, {
+        next: {
+            revalidate: 20,
+        }
+    })
+    const products = await resullt.json();
+    return (
+        <div className="grid grid-cols-12 sm:gap-10">
+            {
+                products.map((product: IProduct, key: string) => <ProductBox key={product._id} {...product} />)
             }
-        })
-        const products = await resullt.json();
-        return (
-            <div className="grid grid-cols-12 sm:gap-10">
-                {
-                    products.map((product: IProduct, key: string) => <ProductBox key={product._id} {...product} />)
-                }
-            </div>
-        )
-    }
+        </div>
+    )
+}
 
 export default ProductsContent
