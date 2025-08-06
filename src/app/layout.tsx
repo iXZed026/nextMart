3094d95b77bfb6c05d0f9c06d544c50baa586ed6
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-06 14:50:40 +0330

    feat: add open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,22 @@ export const metadata: Metadata = {
     default: "NextMart",
     template: "NextMart | %s"
   },
-  icons : "/images/next_mart-logo.png"
-};
+  icons: "/images/next_mart-logo.png",
+  openGraph: {
+    title: "NextMart | Modern E-commerce App",
+    description: "Buy your favorite products with ease – powered by Next.js and MongoDB.",
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    siteName: "NextMart",
+    images: [
+      {
+        url: "/images/next_mart-logo.png",
+        width: 630,
+        height: 630,
+        alt: "NextMart Banner",
+      },
+    ],
+  }
+}
 
 export default function RootLayout({
   children,
